5b4c00b0e2b09ad21699ef5073e2bfdd87b7e666
ojas98Byjus <ojas.balsotra4@byjus.com>
2023-12-20 17:37:56 +0530

    V0Inprogress

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -11,6 +11,13 @@ const Chatbot: React.FC = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [userInput, setUserInput] = useState<string>("");
 
+  const computerOperators = ["Aditi", "Soutik", "Steven", "Lesley"];
+
+  const getRandomOperator = () => {
+    const randomIndex = Math.floor(Math.random() * computerOperators.length);
+    return computerOperators[randomIndex];
+  };
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(e.target.value);
   };
@@ -23,10 +30,19 @@ const Chatbot: React.FC = () => {
       // Simulate computer response after a delay (you can customize the delay)
       setTimeout(() => {
         const computerResponse: MessageType = {
-          text: "Computer response...",
+          text: `${getRandomOperator()} is typing...`,
           sender: "computer",
         };
         setMessages((prevMessages) => [...prevMessages, computerResponse]);
+
+        // Simulate another delay before the actual response
+        setTimeout(() => {
+          const actualResponse: MessageType = {
+            text: `Hi! I'm ${getRandomOperator()} from BYJU’S. I am here to help you.`,
+            sender: "computer",
+          };
+          setMessages((prevMessages) => [...prevMessages, actualResponse]);
+        }, 1000);
       }, 500);
     }
     setUserInput("");
@@ -40,9 +56,10 @@ const Chatbot: React.FC = () => {
 
   useEffect(() => {
     // Initial computer message
+    const initialOperator = getRandomOperator();
     setMessages([
       {
-        text: "Hi! I'm Aditi from BYJU’S. I am here to help you book your free demo class.",
+        text: `Hi! I'm ${initialOperator} from BYJU’S. I am here to help you book your free demo class. `,
         sender: "computer",
       },
     ]);
