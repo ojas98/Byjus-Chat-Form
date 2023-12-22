bef119d15d2769520d96326e7267e4c46dc1cede
ojas98Byjus <ojasupgradds@gmail.com>
2023-12-22 15:37:49 +0530

    V0 InProgress

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Chatbot from "./components/Chatbot.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <Chatbot />
   </React.StrictMode>
 );
