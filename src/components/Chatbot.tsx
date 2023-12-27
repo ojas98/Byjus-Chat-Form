9f3d0dfb21c534fb20ad0180f569f50cc6e6d7a8
ojas98Byjus <ojasupgradds@gmail.com>
2023-12-27 13:45:18 +0530

    Added a random bot selector.

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,10 +1,11 @@
 // Chatbot.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card/Card";
 import CardHeader from "./Card/CardHeader";
 import CardBody from "./Card/CardBody";
 import { UserData, UserDataContextProvider } from "../contexts/UserDataContext";
 import MessageController from "./Message/MessageController";
+import { BotContextProvider, bots } from "../contexts/BotContext";
 
 const G_FORM_APP =
   "https://script.google.com/macros/s/AKfycbwo8hvSpvR3gKxkU94BQ_pzPYmNde3uvl_9RZr54aVYu8vUUt99BkG72t2ase5sIexIjg/exec";
@@ -37,22 +38,35 @@ const Chatbot: React.FC = () => {
     date: "",
     time: "",
     question: "",
+    phone: "",
   });
+
+  const [bot, setBot] = useState(bots[0]);
+
+  useEffect(() => {
+    setBot(bots[Math.floor(Math.random() * bots.length)]);
+  }, []);
+
+  console.log(bot);
 
   return (
     <UserDataContextProvider value={{ data, setData }}>
-      <Card>
-        <CardHeader>
-          <div className="text-brand font-bold text-3xl">Book a Demo Class</div>
-        </CardHeader>
-        <CardBody>
-          <MessageController
-            onComplete={() => {
-              submitData(data);
-            }}
-          />
-        </CardBody>
-      </Card>
+      <BotContextProvider value={bot}>
+        <Card>
+          <CardHeader>
+            <div className="text-brand font-bold text-3xl">
+              Book a Demo Class
+            </div>
+          </CardHeader>
+          <CardBody>
+            <MessageController
+              onComplete={() => {
+                submitData(data);
+              }}
+            />
+          </CardBody>
+        </Card>
+      </BotContextProvider>
     </UserDataContextProvider>
   );
 };
--- src/contexts/BotContext/index.ts
+++ src/contexts/BotContext/index.ts
@@ -1,9 +1,26 @@
 import React from "react";
-import customImage from "../BotContext/lesley.png";
+import lesleyImg from "./lesley.png";
+import soutikImg from "./soutik.png";
+import stevenImg from "./steven.png";
+
+export const bots = [
+  {
+    name: "Lesley",
+    avatar: lesleyImg,
+  },
+  {
+    name: "Soutik",
+    avatar: soutikImg,
+  },
+  {
+    name: "Steven",
+    avatar: stevenImg,
+  },
+];
 
 const botContext = React.createContext({
   name: "Lesley",
-  avatar: customImage,
+  avatar: lesleyImg,
 });
 
 export function useBotContext() {
